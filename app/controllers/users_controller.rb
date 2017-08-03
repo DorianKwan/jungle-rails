a26275aaaa01838ccd538ad693a14d0de96bf9a7
Bryce <bryce.sayers-kwan@live.ca>
2017-08-03 20:55:37 +0000

    Added logic for successful save of new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,16 @@
 class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
+
+    if @user.save
+      redirect_to [:products], notice: 'Successfully Registered!'
+    else
+      render :new
+    end
+  end
+
+  def new
+    @user = User.new
   end
 
   private
