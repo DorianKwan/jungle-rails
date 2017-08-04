772734bf7c6dd77a20cf73b5fbc3b94244731437
Bryce <bryce.sayers-kwan@live.ca>
2017-08-04 18:53:54 +0000

    Added a flash message for invalid password

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,7 @@ class SessionsController < ApplicationController
       session[:user_id] = user.id
       redirect_to '/'
     else
+      flash[:notice] = 'Invalid user email or password.'
       redirect_to '/login'
     end
 
