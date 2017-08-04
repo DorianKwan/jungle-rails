5684cfe9e4e659b16a09310205d0df1519b558a4
Bryce Sayers-Kwan <bryce.sayers-kwan@live.ca>
2017-08-04 16:03:39 -0700

    Added validations to review model

---
+++ app/models/review.rb
@@ -0,0 +1,9 @@
+class Review < ActiveRecord::Base
+  belongs_to :product_id
+  belongs_to :user_id
+
+  validates: :product_id, presence: true
+  validates: :user_id, presence: true
+  validates: :description, presence: true
+  validates: :rating, presence: true
+end
