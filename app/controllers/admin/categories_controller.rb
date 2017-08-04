0e66dcc93eb92cc6d5718484e75b33082aa039d3
Bryce Sayers-Kwan <bryce.sayers-kwan@live.ca>
2017-08-04 13:50:56 -0700

    Added basic http auth on admin pages

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,5 +1,7 @@
 class Admin::CategoriesController < ApplicationController
 
+   http_basic_authenticate_with name: ENV['USER_AUTH_NAME'], password: ENV['USER_AUTH_PASSWORD']
+
   def index
     @categories = Category.order(id: :desc).all
   end
