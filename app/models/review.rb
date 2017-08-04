491e9ef10ba4306a68ddb6750d1486d4b7131d07
Bryce Sayers-Kwan <bryce.sayers-kwan@live.ca>
2017-08-04 16:22:49 -0700

    Fixed validations for review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,8 +2,8 @@ class Review < ActiveRecord::Base
   belongs_to :product_id
   belongs_to :user_id
 
-  validates: :product_id, presence: true
-  validates: :user_id, presence: true
-  validates: :description, presence: true
-  validates: :rating, presence: true
+  validates :product_id, presence: true
+  validates :user_id, presence: true
+  validates :description, presence: true
+  validates :rating, presence: true
 end
