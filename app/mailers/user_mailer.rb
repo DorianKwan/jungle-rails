7d54b2226a8fe6abf12cc7a4ea4c9bbad77fee3b
Bryce <bryce.sayers-kwan@live.ca>
2017-08-04 06:07:38 +0000

    Added email preview and some minor character additions

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,5 +3,6 @@ class UserMailer < ApplicationMailer
 
   def order_email(order)
     @order = order
-    mail(to: @user.email, subject: "Order #{@order.id} Confirmation from Jungle")
+    mail(to: @order.email, subject: "Order #{@order.id} Confirmation from Jungle")
+  end
 end
