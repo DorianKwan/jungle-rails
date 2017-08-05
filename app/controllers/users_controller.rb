f7d896777374e75740594d173a47a9b5862b5b54
Bryce Sayers-Kwan <bryce.sayers-kwan@live.ca>
2017-08-05 00:47:38 -0700

    User is logged in after registration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,9 +4,10 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
+      session[:user_id] = @user.id
       redirect_to [:products], notice: 'Successfully Registered!'
     else
-      render :new
+      render :new, flash: { error: 'Registration Unsuccessful' }
     end
   end
 
