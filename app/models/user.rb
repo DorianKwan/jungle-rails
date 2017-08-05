d7da0d75db0a84875affce60ded75413ee9f3d74
Bryce Sayers-Kwan <bryce.sayers-kwan@live.ca>
2017-08-05 14:36:24 -0700

    Fixed password confirmation

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,5 +8,6 @@ class User < ActiveRecord::Base
   validates :last_name, presence: true
   validates :email, presence: true, uniqueness: { case_sensitive: false }
   validates :password, confirmation: true
+  validates :password_confirmation, presence: true
 
 end
