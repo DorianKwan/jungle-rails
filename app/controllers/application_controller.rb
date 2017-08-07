5068c022546a82397e081b4728fa546cce65a87e
Bryce Sayers-Kwan <bryce.sayers-kwan@live.ca>
2017-08-07 16:27:31 -0700

    Fixed bug on navbar showing logout in signup page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,7 +20,7 @@ class ApplicationController < ActionController::Base
   end
 
   def current_user
-    @user ||= User.find_by_id(session[:user_id])
+    @user = User.find_by_id(session[:user_id])
   end
   helper_method :current_user
 end
